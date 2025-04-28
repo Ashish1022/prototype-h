a450744055f95f2692c33f66cb92991d3b094829
Ashish <ashishjadhav9900@gmail.com>
2025-04-28 23:16:22 +0530

    home-layout

---
+++ src/app/(home)/layout.tsx
@@ -0,0 +1,19 @@
+import { Footer } from "@/modules/home/ui/components/footer";
+import { Navbar } from "@/modules/home/ui/components/navbar";
+
+export default function RootLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div className='flex flex-col min-h-screen'>
+             <Navbar />
+             <div className='flex-1 bg-[#f4f4f0]'>
+                {children}
+            </div>
+            <Footer />
+        </div>
+
+    );
+};

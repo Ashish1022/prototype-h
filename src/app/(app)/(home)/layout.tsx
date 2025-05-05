ac9a9b750b4033486430a6fa04062353686bc0a4
Ashish1022 <ashishjadhav9900@gmail.com>
2025-05-05 22:35:14 +0530

    categories fetch bug fixing

--- src/app/(app)/(home)/layout.tsx
+++ src/app/(app)/(home)/layout.tsx
@@ -1,17 +1,30 @@
 import { Footer } from "@/modules/home/ui/components/footer";
 import { Navbar } from "@/modules/home/ui/components/navbar";
-import { SearchFilters } from "@/modules/home/ui/components/search-filters";
+import { SearchFilters, SearchFiltersSkeleton } from "@/modules/home/ui/components/search-filters";
+import { getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
+import { Suspense } from "react";
 
 export default function RootLayout({
     children,
 }: Readonly<{
     children: React.ReactNode;
 }>) {
+
+    const queryClient = getQueryClient();
+    void queryClient.prefetchQuery(
+        trpc.categories.getMany.queryOptions()
+    );
+
     return (
         <div className='flex flex-col min-h-screen'>
-             <Navbar />
-             <SearchFilters/>
-             <main className='flex-1 bg-[#f4f4f0]'>
+            <Navbar />
+            <HydrationBoundary state={dehydrate(queryClient)}>
+                <Suspense fallback={<SearchFiltersSkeleton />}>
+                    <SearchFilters />
+                </Suspense>
+            </HydrationBoundary>
+            <main className='flex-1 bg-[#f4f4f0]'>
                 {children}
             </main>
             <Footer />
